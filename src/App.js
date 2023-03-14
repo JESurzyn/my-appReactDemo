1a614ba6bf935d8cc5810223d22df2bc426c1d33
JESurzyn <jordan.surzyn@gmail.com>
2023-03-14 08:27:20 -0400

    react day 3

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import Navbar from './Navbar';
 import Greeter from './Greeter';
 import Counter from './Counter';
 import MoodToggler from './MoodToggler';
+import Cart from './Cart';
 import './App.css';
+
+const items = [
+  { id: 1, name: 'Taco Seasoning', price: 2.25, qty: 2 },
+  { id: 2, name: 'Pinto Beans', price: 1.99, qty: 3 },
+  { id: 3, name: 'Sour Cream', price: 3.50, qty: 1 }
+]
 
 function App() {
   return (
     <div>
-      <Counter step={5}/>
+      <Cart items={items} />
+      <Counter step={5} />
       <Counter />
       <MoodToggler />
       <Navbar />
---
+++ src/Cart.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import CartItem from './CartItem';
+
+function Cart({ items }) {
+    const grandTotal = items.reduce((total, item) => (
+        total + item.qty * item.price
+    ), 0).toFixed(2);
+    return (
+        <div className="Cart">
+            <h1>I AM CART</h1>
+            <div className="Cart-items">
+                {items.map(item => (
+                    <CartItem key={item.id} {...item} />
+                ))}
+            </div>
+            <h2>Grand Total: ${grandTotal}</h2>
+        </div>
+    )
+}
+
+export default Cart;
